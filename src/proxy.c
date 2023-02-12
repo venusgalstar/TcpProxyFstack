478256c0a7d202f138396950009f5a67f16939d3
= <=>
2023-02-12 18:16:30 +0100

    completed parse options funcion

--- src/proxy.c
+++ src/proxy.c
@@ -3,11 +3,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
-
+#include <stdlib.h>
+#include <stdarg.h>
 
 int parse_options(int argc, char *argv[]);
 
 char *bind_addr, *remote_host, *cmd_in, *cmd_out;
+int remote_port;
+bool foreground = FALSE;
+bool use_syslog = FALSE;
 
 int main(int argc, char *argv[]){
     int local_port;
@@ -24,10 +28,38 @@ int parse_options(int argc, char *argv[]){
     while((c = getopt(argc, argv, "b:l:h:p:i:o:fs")) != -1 ){
         switch (c)
         {
-        case 'L':
+        case 'l':
             local_port = atoi(optarg);
             break;
         
+        case 'b':
+            bind_addr = optarg;
+            break;
+
+        case 'h':
+            remote_host = optarg;
+            break;
+
+        case 'p':
+            remote_port = atoi(optarg);
+            break;
+
+        case 'i':
+            cmd_in = optarg;
+            break;
+
+        case '0':
+            cmd_out = optarg;
+            break;
+
+        case 'f':
+            foreground = TRUE;
+            break;
+        
+        case 's':
+            use_syslog = TRUE;
+            break;
+
         default:
             break;
         }
